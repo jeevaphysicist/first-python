05f03023606da58304a65e5a97628b3d0ffc1c05
jeevaphysicist <jeevaphysicist@gmail.com>
2023-05-04 12:39:54 +0530

    code

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ def index():
     return "Hello, World!"
 
 
-    # Read the image from the request body
-    image = request.files["image"].read()
-
-    # Create a 3D model from the image
-    model = create_model_from_image(image)
-
-    # Save the 3D model
-    with open("model.fbx", "wb") as f:
-        f.write(model)
-
-    return {"message": "The 3D model has been created."}
 
 if __name__ == "__main__":
     app.run()
